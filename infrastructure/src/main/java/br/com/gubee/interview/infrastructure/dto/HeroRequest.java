ceb80b0fc5dc600fe46899cbd9b0992b35022739
thiagoocms <thiago.mendes@mv.com.br>
2024-03-29 22:43:22 -0300

    reff

--- infrastructure/src/main/java/br/com/gubee/interview/infrastructure/dto/HeroRequest.java
+++ infrastructure/src/main/java/br/com/gubee/interview/infrastructure/dto/HeroRequest.java
@@ -1,6 +1,5 @@
 package br.com.gubee.interview.infrastructure.dto;
 
-import br.com.gubee.interview.core.domain.PowerStats;
 import br.com.gubee.interview.core.domain.enums.HeroRaceEnum;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -17,8 +16,6 @@ public class HeroRequest implements Serializable {
     private UUID heroId;
     private String name;
     private HeroRaceEnum race;
-    private PowerStats powerStats;
-    private boolean enabled;
     private int strength;
     private int agility;
     private int dexterity;
--- infrastructure/src/main/java/br/com/gubee/interview/infrastructure/dto/HeroResponse.java
+++ infrastructure/src/main/java/br/com/gubee/interview/infrastructure/dto/HeroResponse.java
@@ -1,6 +1,5 @@
 package br.com.gubee.interview.infrastructure.dto;
 
-import br.com.gubee.interview.core.domain.PowerStats;
 import br.com.gubee.interview.core.domain.enums.HeroRaceEnum;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -17,7 +16,6 @@ public class HeroResponse implements Serializable {
     private UUID heroId;
     private String name;
     private HeroRaceEnum race;
-    private PowerStats powerStats;
     private int strength;
     private int agility;
     private int dexterity;
